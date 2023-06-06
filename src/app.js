6b4fd1d78d0d84ed3bb437371b4d6ad56fc95c1f
gabydavilaneves@gmail.com <gabydavilaneves@gmail.com>
2023-06-06 14:56:05 -0300

    docs: Melhorando a documentação do projeto

--- src/app.js
+++ src/app.js
@@ -7,56 +7,57 @@ app.use(express.json())
 
 //mock
 const selecoes = [
-    { id: 1, selecao: 'Brasil', grupo: 'G' },
-    { id: 2, selecao: 'Suiça', grupo: 'G' },
-    { id: 3, selecao: 'Servia', grupo: 'G' },
-    { id: 4, selecao: 'Camarões', grupo: 'G' }
+  { id: 1, selecao: 'Brasil', grupo: 'G' },
+  { id: 2, selecao: 'Suiça', grupo: 'G' },
+  { id: 3, selecao: 'Servia', grupo: 'G' },
+  { id: 4, selecao: 'Camarões', grupo: 'G' }
 ]
 
 
 //Retornar o objeto por id
 function buscarSelecaoPorId(id) {
-    return selecoes.filter(selecao => selecao.id == id)
-}
+  return selecoes.filter(selecao => selecao.id == id)
+} //Filtrando por seleção dentro da lista de seleções, caso o id seja igual o da lista, retornara a seleção do id 
 
 //Buscar posicao do elemento no array por id
 function buscaIndexSelecao(id) {
-    return selecoes.findIndex(selecao => selecao.id == id) //retorna o indice do id que o usuario está buscando
+  return selecoes.findIndex(selecao => selecao.id == id) //retorna o indice do id que o usuario está buscando
 }
 
 //criar rota padrão
 app.get('/', (req, res) => {
-    res.send("Pagina inicial")
+  res.send("Pagina inicial")
 }); //ENDPOINT:Parte final da url. Ex: /filmes, /jogos
 
 app.get('/selecoes', (req, res) => {
-    res.status(200).send(selecoes)
+  res.status(200).send(selecoes)
 })
 
 
 //Get com opção de busca por id
 app.get('/selecoes/:id', (req, res) => {
-    res.json(buscarSelecaoPorId(req.params.id))
+  res.json(buscarSelecaoPorId(req.params.id))
 })
 
+//Metodo para criar nova seleção
 app.post('/selecoes', (req, res) => {
-    selecoes.push(req.body) //req.body = corpo da requisão
-    res.status(201).send('Seleção cadastrada com sucesso!')
+  selecoes.push(req.body) //req.body = corpo da requisão
+  res.status(201).send('Seleção cadastrada com sucesso!')
 })
 
 //Delete por id
 app.delete('/selecoes/:id', (req, res) => {
-    let index = buscaIndexSelecao(req.params.id)
-    selecoes.splice(index, 1) //Remove elemento do array
-    res.status(200).send(`Selecão deletada com o ${req.params.id} deletada sucesso!`)
+  let index = buscaIndexSelecao(req.params.id)
+  selecoes.splice(index, 1) //Remove elemento do array
+  res.status(200).send(`Selecão deletada com o ${req.params.id} deletada sucesso!`)
 })
 
 //Atualiza por id
 app.put('/selecoes/:id', (req, res) => {
-    let index = buscaIndexSelecao(req.params.id)
-    selecoes[index].selecao = req.body.selecao
-    selecoes[index].grupo = req.body.grupo
-    res.json(selecoes)
+  let index = buscaIndexSelecao(req.params.id)
+  selecoes[index].selecao = req.body.selecao
+  selecoes[index].grupo = req.body.grupo
+  res.json(selecoes)
 })
 
 export default app //Exportação padrão é o app 
