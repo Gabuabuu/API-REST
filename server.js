a8e6e5bde2a4522a7d5730d3f65d00e645de917a
Gabuabuu <gabydavilaneves@gmail.com>
2023-06-06 22:29:16 -0300

    Revert "feat: Implementando mysql como banco de dados"
    This reverts commit a9d06590eb7bc477c3c4058cf043b180c4f4f124.

--- server.js
+++ server.js
@@ -1,18 +1,7 @@
 import app from "./src/app.js"; //Importando o "app" do app.js
 
-import conexao from "./infra/conexao.js";
-
 const PORT = 2811; //Endereço da porta do server
 
-//Fazer conexão 
-conexao.connect((erro) => {
-    if (erro) {
-        console.log(erro)
-    } else {
-        console.log('Conexão realizada com sucesso!');
-
-        app.listen(PORT, () => {
-            console.log(`Servidor rodando na porta ${PORT}`)
-        })
-    }
+app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`)
 })
